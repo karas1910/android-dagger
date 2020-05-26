57ec22f42d7bfc80ae03bd7d714c6e045c01a096
karas1910 <onenight1910@gmail.com>
2020-05-26 16:27:22 +0900

    done all

--- app/src/main/java/com/example/android/dagger/di/StorageModule.kt
+++ app/src/main/java/com/example/android/dagger/di/StorageModule.kt
@@ -1,13 +1,19 @@
 package com.example.android.dagger.di
 
+import android.content.Context
 import android.content.SharedPreferences
 import com.example.android.dagger.storage.SharedPreferencesStorage
 import com.example.android.dagger.storage.Storage
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 
 @Module
-abstract class StorageModule {
-    @Binds
-    abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+class StorageModule {
+    // @Binds
+    // abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
+    @Provides
+    fun provideStorage(context: Context): Storage {
+        return SharedPreferencesStorage(context)
+    }
 }
